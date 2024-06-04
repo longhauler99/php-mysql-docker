404d25112967f3bf36ca85f38033626a362f01e2
Timothy Saina <devsainar@gmail.com>
2024-06-05 00:42:29 +0300

    ...

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
+} 
 
 // SQL query to fetch data from the table
 $sql = "SELECT * FROM feedback"; // Change 'your_table' to your table name
